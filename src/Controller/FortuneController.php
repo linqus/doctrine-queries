a5a0a37e74a78ef7aaae314bbdf1a9d6ed748393
Karol Linkiewicz <karol@linkiewicz.pl>
2023-06-28 22:25:52 +0200

    enable filter in controller

--- src/Controller/FortuneController.php
+++ src/Controller/FortuneController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\FortuneCookieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,9 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FortuneController extends AbstractController
 {
+
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+
+    }
+
     #[Route('/', name: 'app_homepage')]
     public function index(CategoryRepository $categoryRepository, Request $request): Response
     {
+
+        $this->entityManager->getFilters()
+            ->enable('fortuneCookie_discontinued')
+            //->setParameter('discontinued', true)
+        ;
+
         $searchString = $request->query->get('q');
 
         if ($searchString) {
